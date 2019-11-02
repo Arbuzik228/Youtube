c667d344dca6e2683f3337a736dc7ef0858143b5
Admin <detgo6587@gmail.com>
2019-11-02 12:39:13 +0200

    4545

---
+++ src/main/java/Footoorama.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Admin on 02.11.2019.
+ */
+public class Footoorama {
+    public static void main(String args []) {
+        String goodluck ="green";
+        switch (goodluck) {
+            case "green":System.out.println(goodluck);
+                break;
+            case "yellow":System.out.println("yellow");
+                break;
+            case "red":System.out.println("red");
+                break;
+
+            default:
+                System.out.println("default");
+                break;
+        }
+    }
+}
